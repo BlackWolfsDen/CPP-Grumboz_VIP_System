271bcb2bf873d056c30fec2e32a948fcd084ba71
Grumbo <blackwolfsden@hotmail.com>
2018-02-22 20:21:44 -0700

    Update Item DB Edits.sql

--- Engine/sql updates/Required DB Edits/Item DB Edits.sql
+++ Engine/sql updates/Required DB Edits/Item DB Edits.sql
@@ -1,4 +1,4 @@
 -- Add these coulmns to your world.items_template DB
 
 ALTER TABLE `item_template`
-	ADD COLUMN `vip` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' AFTER `VerifiedBuild`,
+	ADD COLUMN `vip` TINYINT(3) UNSIGNED NOT NULL DEFAULT '1' AFTER `VerifiedBuild`;
